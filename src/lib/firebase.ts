f5bc357d0d26cbbca26bd5efd0a3de9ccd244d09
Larissa de Souza Crispim <larissa.crispim@unesp.br>
2025-09-12 14:50:10 +0000

    Na ABA "Linha de Produção" Conecte-se ao Realtime Database do projeto Fa

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,13 +6,13 @@ import { getDatabase } from "firebase/database";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: "AIzaSyDc-4wulk7iAeKbLs2sH7kuIevYb-VDwn0",
+  authDomain: "fabritrack-a839f.firebaseapp.com",
+  databaseURL: "https://fabritrack-view-default-rtdb.firebaseio.com/",
+  projectId: "fabritrack-a839f",
+  storageBucket: "fabritrack-a839f.appspot.com",
+  messagingSenderId: "1070180776404",
+  appId: "1:1070180776404:web:324d335c9f454282b545ab"
 };
 
 // Initialize Firebase
