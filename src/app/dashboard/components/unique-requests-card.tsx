1107603c55938dc3ca355b96fd22347de745f868
Larissa Crispim <larissacrispim15@gmail.com>
2025-09-18 01:00:36 +0000

    mude o nome 'Requisições Únicas' para 'Número de Requisições Planejadas'

--- src/app/dashboard/components/unique-requests-card.tsx
+++ src/app/dashboard/components/unique-requests-card.tsx
@@ -22,7 +22,7 @@ export function UniqueRequestsCard({ records, className }: UniqueRequestsCardPro
     <Card className={cn(className)}>
       <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
         <CardTitle className="text-sm font-medium">
-          Requisições Únicas
+          Número de Requisições Planejadas
         </CardTitle>
         <FileText className="h-4 w-4 text-muted-foreground" />
       </CardHeader>
