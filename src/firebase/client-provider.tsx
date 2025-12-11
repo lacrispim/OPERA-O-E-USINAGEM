b121f4697d1a46bc95ae3ca4cd0f63257b567a8d
Larissa Crispim <larissacrispim15@gmail.com>
2025-12-11 18:48:46 +0000

    Pode tirar a tela de login. Deixe apenas as duas telas "Registro de Prod

--- src/firebase/client-provider.tsx
+++ src/firebase/client-provider.tsx
@@ -4,17 +4,11 @@
 import { ReactNode } from 'react';
 import { initializeFirebase } from './index';
 import { FirebaseProvider } from './provider';
-import { useUser } from './auth/use-user';
-import { usePathname } from 'next/navigation';
 
 // Initialize Firebase on the client
 const { firebaseApp, firestore, auth, database } = initializeFirebase();
 
 export const FirebaseClientProvider = ({ children }: { children: ReactNode }) => {
-  // We still use useUser to initialize the auth state listener,
-  // but the redirection logic is now handled by the middleware.
-  useUser();
-
   return (
     <FirebaseProvider
       firebaseApp={firebaseApp}
